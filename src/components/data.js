368d51ad11a894f9812c5a166bfe83643f0fc854
MaimouniMohamedAmine <maimouni.mohamedamine.solicode@gmail.com>
2022-05-18 13:54:32 +0100

    main

---
+++ src/components/data.js
@@ -0,0 +1,17 @@
+const Data = [
+  {
+    id: 1,
+    name: "Task",
+    status: "todo",
+  },
+  {
+    id: 2,
+    name: "Task",
+    status: "todo",
+  },
+  {
+    id: 3,
+    name: "Task",
+    status: "todo",
+  },
+];
